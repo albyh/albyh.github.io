baaf67790b341102ed1ed62d0d462bed39f6f8fc
albyh <albyheredia@gmail.com>
2016-06-20 08:30:17 -0700

    Fixed linting errors.

--- js/portfolio.js
+++ js/portfolio.js
@@ -12,27 +12,26 @@
         }).appendTo('#terminal');
 
 
-        setTimeout( function() { $('h1').remove(); }, 495 );
+        setTimeout(function () { $('h1').remove(); }, 495);
 
         printHome();
     }
 
-    
     function printHome() {
                     //text | cumulative delay | class to appendTo | Id
         var text = [
-                   ["c:\\> load alby-heredia.rom",500, 'terminal', ''],
-                   ["Bootstrapping...", 1000, 'terminal', ''],
-                   ["Start-up Self Check... ", 1250, 'terminal', ''],
-                   ["COMPLETE", 2500, 'terminal', ''],
-                   ["WELCOME TO Alby Heredia's Portfolio Website!", 2600, 'terminal', ''],
-                   ["Please select an option:", 2700, 'terminal', ''],
-                   ["1. Reboot", 2800, 'terminal pipmenu', 'pip1'],
-                   ["2. About Alby", 2800, 'terminal pipmenu', 'pip2'],
-                   ["3. Portfolio Playground", 2800, 'terminal pipmenu', 'pip3'],
-                   ["4. Contact Alby", 2850, 'terminal pipmenu', 'pip4'],
-                   [">_", 2850, 'terminal', '']
-                   ];
+                ["c:\\> load alby-heredia.rom", 500, 'terminal', ''],
+                ["Bootstrapping...", 1000, 'terminal', ''],
+                ["Start-up Self Check... ", 1250, 'terminal', ''],
+                ["COMPLETE", 2500, 'terminal', ''],
+                ["WELCOME TO Alby Heredia's Portfolio Website!", 2600, 'terminal', ''],
+                ["Please select an option:", 2700, 'terminal', ''],
+                ["1. Reboot", 2800, 'terminal pipmenu', 'pip1'],
+                ["2. About Alby", 2800, 'terminal pipmenu', 'pip2'],
+                ["3. Portfolio Playground", 2800, 'terminal pipmenu', 'pip3'],
+                ["4. Contact Alby", 2850, 'terminal pipmenu', 'pip4'],
+                [">_", 2850, 'terminal', '']
+            ];
 
         $.each(text, function (i, str) {
 
@@ -41,20 +40,18 @@
                     'text': str[0],
                     'class': str[2],
                     'id': str[3] == '' ? '' : str[3]
-                }).appendTo('#terminal')
-            }, str[1])
+                }).appendTo('#terminal');
+            }, str[1]);
         });
     }
 
-    function scrollTo( el ){
+    function scrollTo(el) {
         el.scrollIntoView(true);
     }
 
     //event handlers
     $('body').on('click', function (e) {
         console.log("menu: " + e.target.id);
-
-        var scrollto = '';
 
         if (e.target.id === 'pip1') {
             scrollTo(document.getElementById("home"));
@@ -70,8 +67,6 @@
 
     $("body").keypress(function (e) {
         console.log(e.which)
-
-        var scrollto = '';
 
         if (e.which === 49) {
             scrollTo(document.getElementById("home"));
